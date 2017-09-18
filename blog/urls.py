0107abaedff0552f9aa1c51732933c796035ecc2
JeffRisberg <jrisberg@aol.com>
2017-09-17 23:01:38 -0700

    added tag detail.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
-from views import homepage
+from views import homepage, tag_detail
 
 urlpatterns = [
+    url(r'^tag/(?P<slug>[\w\-]+)/$',
+        tag_detail,
+        ),
     url(r'^$', homepage),
 ]
